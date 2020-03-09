767cc25f150d86d3ef1bf7501b434e44bb1e079f
Amr-Aboshama <31421825+Amr-Aboshama@users.noreply.github.com>
2020-03-09 23:15:45 +0200

    Data Keeper Full Stream
    Working:
    - Uploading
    - Downloading
    - Replicating

--- data_keeper/data_keeper.py
+++ data_keeper/data_keeper.py
@@ -1,21 +1,73 @@
 import zmq
 import cv2
+import sys
+import os
 
+
+'''
+Download:
+    *Request: FILE_NAME
+    *Response: FILE_NAME, FPS, FOURCC, WIDTH, HEIGHT, COUNT, #0, #1, ..., #[COUNT-1]
+
+Upload:
+    *Request: FILE_NAME, FPS, FOURCC, WIDTH, HEIGHT, COUNT, #0, #1, ..., #[COUNT-1]
+'''
 def sendFile(file_name, socket):
-    pass
+    videoData = cv2.VideoCapture('DATA/'+file_name)         #Capture Video
+    fps = videoData.get(cv2.CAP_PROP_FPS)                   #GET FPS
+    fourCC = int(videoData.get(cv2.CAP_PROP_FOURCC))        #GET FOURCC
+    width = int(videoData.get(cv2.CAP_PROP_FRAME_WIDTH))    #GET WIDTH
+    height = int(videoData.get(cv2.CAP_PROP_FRAME_HEIGHT))  #GET HEIGHT
+    count = int(videoData.get(cv2.CAP_PROP_FRAME_COUNT))    #GET FRAMES COUNT
 
-def saveFile(file_name, file_content):
-    pass
+    message = {
+        'FILE_NAME' : file_name,
+        'FPS' : fps,
+        'FOURCC' : fourCC,
+        'WIDTH' : width,
+        'HEIGHT' : height,
+        'COUNT' : count
+    }
+
+
+    for i in range(count):
+        msg.update({("#"+str(i)) : videoData.read()[1]})
+
+    videoData.release()
+
+    socket.send_pyobj(message)
+
+
+
+
+
+def saveFile(video_data):
+    file_name = video_data['FILE_NAME']
+    fps = video_data['FPS']
+    fourCC = video_data['FOURCC']
+    width = video_data['WIDTH']
+    height = video_data['HEIGHT']
+    count = video_data['COUNT']
+
+    out_video = cv2.VideoWriter('DATA/'+file_name, fourCC, fps,(width,height))
+
+    for i in range(count):
+        out_video.write(data[("#"+str(i))])
+        out_video.release()
+
+
+
+
 
 def processStream(message, socket):
-    if message.has_key("PROCESS_TYPE") == False:
+    if ("PROCESS_TYPE") not in message:
         raise NameError("Error in streaming!")
     
     process = message['PROCESS_TYPE']
 
     if process == "Download":
     
-        if message.has_key("FILE_NAME") == False:
+        if ("FILE_NAME") not in message:
             raise NameError("File name is missed!")
 
         sendFile(message['FILE_NAME'],socket)
@@ -28,13 +80,27 @@ def processStream(message, socket):
 
     elif process == "Upload":
         
-        if message.has_key("FILE_NAME") == False:
+        if ("FILE_NAME") not in message:
             raise NameError("File name is missed!")
 
-        if message.has_key("FILE_CONTENT") == False:
-            raise NameError("File content is missed!")
+        if ("FPS") not in message:
+            raise NameError("FPS is missed!")
+        if ("FOURCC") not in message:
+            raise NameError("FOURCC is missed!")
+        if ("WIDTH") not in message:
+            raise NameError("WIDTH is missed!")
+        if ("HEIGHT") not in message:
+            raise NameError("HEIGHT is missed!")
+        if ("COUNT") not in message:
+            raise NameError("COUNT is missed!")
 
-        saveFile(message['FILE_NAME'],message['FILE_CONTENT'])
+        count = message['COUNT']
+
+        for index in range(count):
+            if ("#"+str(index)) not in message:
+                raise NameError("frame #"+str(index)+" is missed!")
+            
+        saveFile(message)
 
         '''
         #TODO
@@ -45,20 +111,23 @@ def processStream(message, socket):
         raise NameError("Error in streaming!")
 
 
+
+
+
 def processReplicate(message, socket, context):
-    if message.has_key("NODE_TYPE") == False:
+    if ("NODE_TYPE") not in message:
         raise NameError("Node type is missed!")
 
     
-    if message['NODE_TYPE'] == "source":
+    if message['NODE_TYPE'] == "Source":
 
-        if message.has_key("FILE_NAME") == False:
+        if ("FILE_NAME") not in message:
             raise NameError("File name is missed!")
     
-        if message.has_key("IP") == False:
+        if ("IP") not in message:
             raise NameError("IP is missed")
 
-        if message.has_key("PORT") == False:
+        if ("PORT") not in message:
             raise NameError("PORT is missed")
             
         temp_socket = context.socket(zmq.PAIR)
@@ -73,7 +142,7 @@ def processReplicate(message, socket, context):
         '''
 
 
-    elif message['NODE_TYPE'] == "destination":
+    elif message['NODE_TYPE'] == "Destination":
 
         received_file = socket.recv_pyobj()
         saveFile(received_file['FILE_NAME'],received_file['FILE_CONTENT'])
@@ -89,9 +158,11 @@ def processReplicate(message, socket, context):
 
 
 
+os.mkdir('DATA')
 
 stream_port = 5556
 #publisher_port here
+
 context = zmq.Context()
 
 stream = context.socket(zmq.PAIR)
@@ -110,6 +181,7 @@ while True:
 
     '''
     #TODO
+    [ONLY RUN ON THE FIRST NODE]
     Heart beats are sent here
     '''
 
