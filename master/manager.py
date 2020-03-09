6a7ff39994e9f57656156d2c218ece56f3c03214
Ayman Azzam <aymanazzam63@gmail.com>
2020-03-09 16:28:01 +0200

    add Configuration file and configure function in master and compile well

--- master/manager.py
+++ master/manager.py
@@ -1,8 +1,9 @@
 import multiprocessing
 import sys
 import zmq
+import random
 
-ip1 = "127.0.0.1";	port = sys.argv[1];	n = sys.argv[2]
+ip1 = "127.0.0.1";	port = int(sys.argv[1]);	n = int(sys.argv[2])
 lookup_table = multiprocessing.Manager().dict()
 available_table = multiprocessing.Manager().dict()
 
@@ -29,20 +30,20 @@ def configure():
 def master_client(port1):
 	my_id = random.randrange(10000)
     
-    context = zmq.Context()
+	context = zmq.Context()
     
-    client = context.socket(zmq.REP)            #REP because it needs to receive then send
-    client.bind("tcp://%s:%s"%(ip1,port1))      #client will connect to this port
+	client = context.socket(zmq.REP)            #REP because it needs to receive then send
+	client.bind("tcp://%s:%i"%(ip1,port1))      #client will connect to this port
 
-    while True:
-        data = client.recv_pyobj()              #Receive message from client 
-        #receiving dictionary contains command(upload/download) and file(file_Data for upload/file_name for download)
-        print("master_client_id %i received command type %s" %(my_id, data['command']))
+	while True:
+		data = client.recv_pyobj()              #Receive message from client 
+		#receiving dictionary contains command(upload/download) and file(file_Data for upload/file_name for download)
+		print("master_client_id %i received command type %s" %(my_id, data['command']))
 		
 		if(data[command]=="upload"):
-			#
+			print("5")
 		elif(data[command]=="download"):
-			#
+			print("6")
 		else:
 			print("master_client_id %i received invalid command" %(my_id))
 
@@ -51,6 +52,6 @@ if __name__ == "__main__":
 		configure()
 
 		for i in range(0,n):
-			p1=	multiprocessing.Process(target=master_client, args=(port))
+			p1=	multiprocessing.Process(target=master_client, args=(port,))
 			p1.start()
 			port = port + 1
