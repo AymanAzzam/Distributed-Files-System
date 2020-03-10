8dcccd4b4723afbdfb2f9e6652bc8a41186f0f2d
Ayman Azzam <aymanazzam63@gmail.com>
2020-03-10 23:24:31 +0200

    added tables for notification ports and publishing ports

--- master/manager.py
+++ master/manager.py
@@ -10,7 +10,9 @@ keepers_num = 0;	processes_num = 0
 replica_factor = 3
 
 lookup_table = multiprocessing.Manager().dict()
-available_table = multiprocessing.Manager().dict()
+available_stream_table = multiprocessing.Manager().dict()
+available_publish_table = multiprocessing.Manager().dict()
+available_notification_table = multiprocessing.Manager().dict()
 ports_list = multiprocessing.Manager().list()
 
 class value:
@@ -37,7 +39,7 @@ def printLookup(proc_num,lookup_table):
 
 def printAvailable(proc_num):
 	print("i am process number : %i inside print Available"  %proc_num)
-	for k, v in available_table.items():
+	for k, v in available_stream_table.items():
 		print(k, v)	
 
 def configure():
@@ -48,12 +50,11 @@ def configure():
 		ip = f.readline().rstrip()			#.rstrip to erase "\n" from the ip end
 		ip_port = int(f.readline())
 		for j in range(0,processes_num):
-			available_table[ip+":"+str(ip_port+j)] = "available"
-			ports_list.append(ip+":"+str(ip_port+j))
+			available_stream_table[ip+":"+str(ip_port+3*j)] = "available"
+			available_publish_table[ip+":"+str(ip_port+3*j+1)] = "available"
+			available_notification_table[ip+":"+str(ip_port+3*j+2)] = "available"
+			ports_list.append(ip+":"+str(ip_port+3*j))
 	return keepers_num, processes_num
-
-from multiprocessing import Lock
-import zmq
 
 def keeper_for_replica(v):
 	index = 0;	i = 0
@@ -77,43 +78,44 @@ def start_index_for_ip(ip):
 def src_dst_port(v):
 	dst_index_start = keeper_for_replica(v);	dst_index = dst_index_start
 	my_mutex.acquire()
-	while(available_table[ports_list[dst_index]] == 'busy'):	#Get available destination port for source to connect on it
+	while(available_stream_table[ports_list[dst_index]] == 'busy'):	#Get available destination port for source to connect on it
 		dst_index = (dst_index + 1) % (dst_index_start + processes_num)
-	available_table[ports_list[dst_index]] = 'busy'
+	available_stream_table[ports_list[dst_index]] = 'busy'
 	
 	src_index_start = start_index_for_ip(v.datakeeper_list[i].split(":")[0]);	src_index = src_index_start
-	while(available_table[ports_list[src_index]] == 'busy'):	#Get available source port for master to connect on it
+	while(available_stream_table[ports_list[src_index]] == 'busy'):	#Get available source port for master to connect on it
 		src_index = (src_index + 1) % (src_index_start + processes_num)
-	available_table[ports_list[src_index]] = 'busy'
+	available_stream_table[ports_list[src_index]] = 'busy'
 	my_mutex.release()
 	return src_index, dst_index
 
-def notify_src_dst(k,src_index,dst_index):
-	message_src = {'NODE_TYPE': "Source", 'FILE_NAME': k, 'IP': ports_list[dst_index].split(":")[0], 'PORT': ports_list[dst_index].split(":")[1]}
-	message_dst = {'NODE_TYPE': "Destination"}
-	socket.connect("tcp://%s"%(ports_list[dst_index]))
-	socket.send_pyobj(message_dst)
-	socket.disconnect()
-	socket.connect("tcp://%s"%(ports_list[src_index]))
-	socket.send_pyobj(message_src)
-	socket.recv_pyobj()
-	socket.disconnect()
-	my_mutex.acquire()
-	available_table[ports_list[dst_index]] = 'available'
-	available_table[ports_list[src_index]] = 'available'
-	my_mutex.release()
+def notify_src_dst(socket1,socket2,k,src_index,dst_index):
+	src_ip =  ports_list[src_index].split(":")[0];	src_port_stream = ports_list[src_index].split(":")[1]
+	dst_ip =  ports_list[dst_index].split(":")[0];	dst_port_stream = ports_list[dst_index].split(":")[1]
+	src_port_notification = str(int(src_port_stream) + 2);	dst_port_notification = str(int(dst_port_stream) + 2)
 
+	message_src = {'NODE_TYPE': "Source", 'FILE_NAME': k, 'IP': dst_ip, 'PORT': dst_port_stream}
+	message_dst = {'NODE_TYPE': "Destination"}
+	socket1.connect("tcp://%s:%s"%(dst_ip,dst_port_notification))
+	socket1.send_pyobj(message_dst)
+	socket1.disconnect()
+	socket2.connect("tcp://%s:%s"%(src_ip,src_port_notification))
+	socket2.send_pyobj(message_src)
+	socket2.disconnect()
+	
 def replica():
 	context = zmq.Context()
 	socket1 = context.socket(zmq.PAIR)
+	socket2 = context.socket(zmq.PAIR)
 
-	for k, v in lookup_table.items():
-		i = 0;	j = 0
-		while(len(v.datakeepers_list)< replica_factor):
-			src_index, dst_index = src_dst_port(v)
-			notify_src_dst(k,src_index,dst_index)
-			replica_factor = replica_factor + 1
-	time.sleep(1)
+	while True:
+		for k, v in lookup_table.items():
+			i = 0;	j = 0
+			while(len(v.datakeepers_list)< replica_factor):
+				src_index, dst_index = src_dst_port(v)
+				notify_src_dst(socket1,socket2,k,src_index,dst_index)
+				replica_factor = replica_factor + 1
+		time.sleep(1)
 
 def master_client(port1):
 	my_id = random.randrange(10000)
@@ -133,9 +135,9 @@ def master_client(port1):
 
 		if(data['PROCESS']=="upload"):
 			my_mutex.acquire()
-			while(available_table[ports_list[starting_dk_port_index]] == "busy"):
+			while(available_stream_table[ports_list[starting_dk_port_index]] == "busy"):
 				starting_dk_port_index=(starting_dk_port_index+1)%(keepers_num*processes_num)
-			available_table[ports_list[starting_dk_port_index]] = "busy"
+			available_stream_table[ports_list[starting_dk_port_index]] = "busy"
 			my_mutex.release()
 			client.send_string(ports_list[starting_dk_port_index])
 
@@ -144,9 +146,9 @@ def master_client(port1):
 			datakeeper_list= val.datakeepers_list
 			my_mutex.acquire()
 			ip_index_temp = start_index_for_ip(datakeeper_list[0].split(":")[0]);	ip_index = ip_index_temp
-			while(availible_table[ports_list[ip_index]] == "busy"):
+			while(available_stream_table[ports_list[ip_index]] == "busy"):
 				ip_index = (ip_index + 1) % (ip_index + processes_num)
-			availible_table[ports_list[ip_index]] = "busy"
+			available_stream_table[ports_list[ip_index]] = "busy"
 			my_mutex.release()
 			client.send_string(ports_list[ip_index])	
 
